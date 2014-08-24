cde434594396f54da83b669a847339520f6a5c7b
Max Makarochkin <mms.post@gmail.com>
2014-08-25 00:22:01 +0400

    first test in detect

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testDetect()
+{
+  touch ${BUILD_DIR}/REQUIRE
+
+  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
+
+  assertEquals 0 ${rtrn}
+  assertEquals "Julia" "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+}
