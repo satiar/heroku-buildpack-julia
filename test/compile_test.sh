7ce54fe593684b2ee6a80d160a36a28e3513ea47
Max Makarochkin <mms.post@gmail.com>
2014-08-25 01:13:54 +0400

    compile julia downloading step and test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testCompile()
+{
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+  assertEquals 0 ${rtrn}
+  assertEquals "" "`cat ${STD_ERR}`"
+
+  assertContains "-----> Downloading Julia 0.3.0"  "`cat ${STD_OUT}`"
+  assertTrue "Should have cached Julia.deb `ls -la ${CACHE_DIR}`" "[ -f ${CACHE_DIR}/julia_0.3.0-1189~ubuntu12.04.1_amd64.deb ]"
+
+  # Run again to ensure cache is used.
+  rm -rf ${BUILD_DIR}/*
+  resetCapture
+
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+  assertNotContains "-----> Downloading Julia 0.3.0"  "`cat ${STD_OUT}`"
+
+  assertEquals 0 ${rtrn}
+  assertEquals "" "`cat ${STD_ERR}`"
+}
