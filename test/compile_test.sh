dd1eb9276fc98f2ceca08efbe4e95e7aacd1efb1
Max Makarochkin <mms.post@gmail.com>
2014-08-25 06:48:14 +0400

    additional compile operations with tests

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -11,6 +11,9 @@ testCompile()
   assertContains "-----> Downloading Julia 0.3.0"  "`cat ${STD_OUT}`"
   assertTrue "Should have cached Julia.deb `ls -la ${CACHE_DIR}`" "[ -f ${CACHE_DIR}/julia_0.3.0-1189~ubuntu12.04.1_amd64.deb ]"
 
+  assertContains "-----> Installing Julia 0.3.0"  "`cat ${STD_OUT}`"
+  assertTrue "Should have installed julia in build dir: `ls -la ${BUILD_DIR}`" "[ -d ${BUILD_DIR}/.julia ]"
+
   # Run again to ensure cache is used.
   rm -rf ${BUILD_DIR}/*
   resetCapture
